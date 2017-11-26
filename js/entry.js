81869c7247ba6727503135d2ed37c21c8bf90c7d
Mateo Zapata <mszado15@gmail.com>
2017-11-25 20:38:53 -0500

    Se ha creado un nuevo mecanismo de interpolacion, ahora entra nodo por nodo ejecutando un metodo recursivo en ves de directamente en el outer TEXT que me genera la API DOM, esto permite a que no se posible insertar textos dentro de las etiquetas HTML

--- js/entry.js
+++ js/entry.js
@@ -1,22 +1,42 @@
 var Lombia = (function() {
     var _d = document;
-    
+    var superData = null;
     function Lombia(app) {
-        var result = this.getElement(app);
+        superData = app.data;
+        var result = this.getElement(app.name);
         if (result === false) {
             console.log(new Error('element no exist'));
         }
         else {
-            this.interpolation(result, app.data);
+            this.getElements(result);
         }
     };
+
+    Lombia.prototype.preIterpolation = function(elem) {
+        if (elem) {
+            if (elem.nodeName === "#text") {
+                
+                this.interpolation(elem, superData.data);
+            }
+            else {
+                this.getElements(elem);
+            }
+        }
+    }
+
+    Lombia.prototype.getElements = function(elements) {
+        if (!elements) return console.error("This element does exist");
+        elements.childNodes.forEach (e => {
+            this.preIterpolation(e);
+        });
+    }
 
     Lombia.prototype.setError = function(message) {
         return document.getElementsByTagName('body')[0].outerHTML = message;
     }
     
-    Lombia.prototype.getElement = function(app) {
-        var el = _d.querySelector("#" + app.name);
+    Lombia.prototype.getElement = function(name) {
+        var el = _d.querySelector("#" + name);
         return el ? el : false;
     }
 
@@ -33,23 +53,34 @@ var Lombia = (function() {
         return false;
     }
 
-    Lombia.prototype.interpolation = function(result, data) {
-        var outhtml = result.outerHTML;
+    Lombia.prototype.GetOnlyInsideTags = function(outhtml = null) {
+        if (outhtml) {
+            var reg = new RegExp(/>\s*(.*)+\s*</);
+            var res = outhtml.match(reg);
+        }
+        return false;
+    }
+
+    Lombia.prototype.interpolation = function(result) {
+        var data = superData;
+        var outhtml = result.textContent;
         var reg = new RegExp(/{{\s*(.*)+\s*}}/g);
         var res = outhtml.match(reg);
-        res.forEach(function(e) {
-            var word = e.replace(/[{}]/g, '');
-            var wordFilter = word.split("|");
-            if (data[wordFilter[0]]) {
-                var convertWord = wordFilter.length === 2 ? this.setFilter(data[wordFilter[0]], wordFilter[1]) : data[wordFilter[0]];
-                outhtml = outhtml.replace(e, convertWord);
-            }
-            else {
-                this.setError('# element no exist');
-                return console.error(new Error('error element ' + word + ' not exist in data'));
-            }
-        }.bind(this));
-        result.outerHTML = outhtml;
+        if (res && data) {
+            res.forEach(function(e) {
+                var word = e.replace(/[{}]/g, '');
+                var wordFilter = word.split("|");
+                if (data[wordFilter[0]]) {
+                    var convertWord = wordFilter.length === 2 ? this.setFilter(data[wordFilter[0]], wordFilter[1]) : data[wordFilter[0]];
+                    outhtml = outhtml.replace(e, convertWord);
+                }
+                else {
+                    this.setError('# element no exist');
+                    return console.error(new Error('error element ' + word + ' not exist in data'));
+                }
+            }.bind(this));
+            result.textContent = outhtml;
+        }
     };
     
     return Lombia;
